ee3f891f6852e0b2939860cbcb35f099d6c244fe
Lorenzo Battiston <lorenzo.battiston@student.htw-berlin.de>
2024-05-17 12:59:30 +0200

    defined abstract graph structure

--- graph.py
+++ graph.py
@@ -2,5 +2,13 @@ from data import ContentNode
 
 
 class Node:
+    """Graph representation of a ContentNode.
+    At the moment more of a type alias for ContentNode"""
     content: ContentNode
-    children: list["Node"]
+
+
+class Edge:
+    """Edge connecting two Nodes.
+    These are calculated using the NodePoints and JunctionPoint"""
+    u: Node
+    v: Node
