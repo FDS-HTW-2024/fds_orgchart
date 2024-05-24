a77f75704d738affcbbe8215243882a75ab66cb5
Lorenzo Battiston <lorenzo.battiston@student.htw-berlin.de>
2024-05-24 15:01:56 +0200

    finding node point

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 import fitz
 import pprint
-from data import Point, Rectangle, TextBlock, ContentNode
+from data import Point, Line, Rectangle, TextBlock, ContentNode
 
 
 def make_text(words):
@@ -36,10 +36,11 @@ drawings_list = page.get_drawings()
 #         print(d["items"][0])
 #         print("---------")
 
-
 rect_list = [x for x in drawings_list if x["items"][0][0] == "re"]
 words = page.get_text("blocks", sort=True)  # list of words on page
 
+lines_list = [x["items"] for x in drawings_list if x["items"][0][0] == "l"]
+print(len(lines_list))
 # create ContentNodes
 for rect in rect_list:
     rect_tuple = rect["items"][0]
@@ -54,5 +55,49 @@ for rect in rect_list:
             Point(x0, y0), Point(x1, y1)), content=word)
         text_blocks.append(text_block)
     content_node = ContentNode(rect=rectangle, content=text_blocks)
-    pprint.pp(content_node)
+    print(content_node)
     print("################")
+
+outpdf = fitz.open()
+outpage = outpdf.new_page(width=page.rect.width, height=page.rect.height)
+shape = outpage.new_shape()
+
+
+def line_intersects(line0, line1):
+
+    return False
+
+
+def get_node_points(rect, line):
+    (x0, y0, x1, y1) = rect
+    (x0, y0, x1, y1) = (x0 - 1, y0 - 1, x1 + 1, y1 + 1)
+
+    topleft_bottomleft = Line(Point(x0, y0), Point(x0, y1))
+    topleft_topright = Line(Point(x0, y0), Point(x1, y0))
+    bottomright_bottomleft = Line(Point(x0, y1), Point(x1, y1))
+    bottomright_topright = Line(Point(x1, y0), Point(x1, y1))
+
+    return (line_intersects(topleft_bottomleft, line) or
+            line_intersects(topleft_topright, line) or
+            line_intersects(bottomright_bottomleft, line) or
+            line_intersects(bottomright_topright, line))
+
+
+for line in lines_list:
+    for line_segment in line:
+        shape.draw_line(line_segment[1], line_segment[2])
+
+for rect in rect_list:
+    shape.draw_rect(rect["items"][0][1])
+    for line in lines_list:
+        for line_segment in line:
+            if get_node_points(rect["items"][0][1],
+                             Line(Point._make(line_segment[1]),
+                                  Point._make(line_segment[2]))):
+                print("intersects")
+                shape.draw_circle(line_segment[1], 4)
+
+
+shape.finish()
+shape.commit()
+outpdf.save("example_orgcharts/reconstructed_lines.pdf")
