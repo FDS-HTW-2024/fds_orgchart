5b7c51d2646fd5df6f97dd2f3e36cddd74d9117a
hummingly <31522351+hummingly@users.noreply.github.com>
2024-05-25 10:45:42 +0200

    Nicer type hints and refactoring out methods on geometry

--- converter.py
+++ converter.py
@@ -1,5 +1,4 @@
 import fitz
-from math import sqrt
 from data import Point, Line, Rectangle, TextBlock, ContentNode
 
 
@@ -27,26 +26,22 @@ doc = fitz.open("example_orgcharts/org_kultur.pdf")
 page = doc[0]  # we want text from this page
 
 drawings_list = page.get_drawings()
-rect_list = list()
+rects: list[Rectangle] = list()
 words = page.get_text("blocks", sort=True)  # list of words on page
-lines_list = list()
+lines: list[Line] = list()
 
 for d in drawings_list:
     for item in d["items"]:
         match item[0]:
             case "re":
-                rect_list.append(item[1])
+                rects.append(Rectangle(top_left=item[1].top_left, bottom_right=item[1].bottom_right))
             case "l":
-                lines_list.append((Point._make(item[1]), Point._make(item[2])))
+                lines.append(Line(p0=Point._make(item[1]), p1=Point._make(item[2])))
             case _:
                 pass
 
-print(len(lines_list))
 # create ContentNodes
-for rect in rect_list:
-    rectangle = Rectangle(
-        top_left=Point(rect.x0, rect.y0),
-        bottom_right=Point(rect.x1, rect.y1))
+for rect in rects:
     rect_words = [w for w in words if fitz.Rect(
         w[:4]).intersects(rect)]
     text_blocks = list()
@@ -54,7 +49,7 @@ for rect in rect_list:
         text_block = TextBlock(bounding_box=Rectangle(
             Point(x0, y0), Point(x1, y1)), content=word)
         text_blocks.append(text_block)
-    content_node = ContentNode(rect=rectangle, content=text_blocks)
+    content_node = ContentNode(rect=rect, content=text_blocks)
     print(content_node)
     print("################")
 
@@ -63,77 +58,41 @@ outpage = outpdf.new_page(width=page.rect.width, height=page.rect.height)
 shape = outpage.new_shape()
 
 
-# Kurbo Library Source: https://github.com/linebender/kurbo/blob/884483b3de412c7c10e2fff4f43dbe96304c0dbd/src/line.rs#L44
-def get_line_intersection(line0, line1, threshold = 5.0):
-    lengthAB = sqrt(pow(line0.p1.x - line0.p0.x, 2) + pow(line0.p1.y - line0.p0.y, 2))
-    lengthCD = sqrt(pow(line1.p1.x - line1.p0.x, 2) + pow(line1.p1.y - line1.p0.y, 2))
+def is_node_point(rect: Rectangle, point: Point, threshold = 1.0):
+    return fitz.Rect(rect.inflate(threshold)).contains(point)
 
-    normalized_ab = Point((line0.p1.x - line0.p0.x) / lengthAB, (line0.p1.y - line0.p0.y) / lengthAB)
-    normalized_cd = Point((line1.p1.x - line1.p0.x) / lengthCD, (line1.p1.y - line1.p0.y) / lengthCD)
-
-    a = Point(line0.p1.x - normalized_ab.x * (lengthAB + threshold), line0.p1.y - normalized_ab.y * (lengthAB + threshold))
-    b = Point(line0.p0.x + normalized_ab.x * (lengthAB + threshold), line0.p0.y + normalized_ab.y * (lengthAB + threshold))
-    c = Point(line1.p1.x - normalized_cd.x * (lengthCD + threshold), line1.p1.y - normalized_cd.y * (lengthCD + threshold))
-    d = Point(line1.p0.x + normalized_cd.x * (lengthCD + threshold), line1.p0.y + normalized_cd.y * (lengthCD + threshold))
-
-    ab = Point(b.x - a.x, b.y - a.y)
-    cd = Point(d.x - c.x, d.y - c.y)
-    ab_cross_cd = ab.x * cd.y - ab.y * cd.x
-
-    if ab_cross_cd == 0.0:
-        return None
-
-    ca = Point(a.x - c.x, a.y - c.y)
-    ab_cross_ca = ab.x * ca.y - ab.y * ca.x
-    cd_cross_ca = cd.x * ca.y - cd.y * ca.x
-    # h and g are the factors how much a point should be moved along the line.
-    g = cd_cross_ca / ab_cross_cd
-    h = ab_cross_ca / ab_cross_cd
-
-    # The values of g and h must be between 0 and 1, otherwise the point will
-    # be outside the lines.
-    if 0.0 <= g <= 1.0 and 0.0 <= h <= 1.0:
-        return Point(c.x + cd.x * h, c.y + cd.y * h)
-
-    return None
-
-
-def is_node_point(rect, point, threshold = 1.0):
-    (x0, y0, x1, y1) = rect
-    rect2 = (x0 - threshold, y0 - threshold, x1 + threshold, y1 + threshold)
-    return fitz.Rect(rect2).contains(point)
 
 node_points = []
 graph_lines = []
 
-for rect in rect_list:
+for rect in rects:
     shape.draw_rect(rect)
-    for line in lines_list:
-        if is_node_point(rect, line[0]):
+    for line in lines:
+        if is_node_point(rect, line.p0):
             print("intersects")
-            shape.draw_circle(line[0], 4)
-            node_points.append(Point._make(line[0]))
-        if is_node_point(rect, line[1]):
+            shape.draw_circle(line.p0, 4)
+            node_points.append(line.p0)
+        if is_node_point(rect, line.p1):
             print("intersects")
-            shape.draw_circle(line[1], 4)
-            node_points.append(Point._make(line[1]))
-        if (not is_node_point(rect, line[0]) and
-                not is_node_point(rect, line[1])):
+            shape.draw_circle(line.p1, 4)
+            node_points.append(line.p1)
+        if (not is_node_point(rect, line.p0) and
+                not is_node_point(rect, line.p1)):
             print("line not in graph")
         else:
-            shape.draw_line(line[0], line[1])
+            shape.draw_line(line.p0, line.p1)
 
 
-for i in range(0, len(lines_list)):
-    line1 = lines_list[i]
-    for j in range(0, len(lines_list)):
-        if i == j:
-            continue
+for i in range(0, len(lines)):
+    line0 = lines[i]
+    for j in range(i + 1, len(lines)):
+        line1 = lines[j]
+        intersection = line0.intersection(line1, 5.0)
 
-        line2 = lines_list[j]
-        intersection = get_line_intersection(Line(line1[0], line1[1]), Line(line2[0], line2[1]))
-        if intersection:
+        if intersection != None:
             print("lines intersect")
+            shape.draw_line(line0.p0, line0.p1)
+            shape.draw_line(line1.p0, line1.p1)
             shape.draw_circle(intersection, 4)
 
 
--- data.py
+++ data.py
@@ -1,20 +1,60 @@
 from dataclasses import dataclass
 from collections import namedtuple
+from math import sqrt
+import typing
 
 Point = namedtuple("Point", ["x", "y"], defaults=[0.0, 0.0])
 
 
-@dataclass
-class Rectangle:
+class Rectangle(typing.NamedTuple):
     """Represents a Rectangle in the orgchart"""
     top_left: Point
     bottom_right: Point
 
+    def inflate(self, value: float):
+        return Rectangle(
+            top_left=Point(self.top_left.x - value, self.top_left.y - value),
+            bottom_right=Point(self.bottom_right.x + value, self.bottom_right.y + value)
+        )
 
-@dataclass
-class Line:
+
+class Line(typing.NamedTuple):
     p0: Point
     p1: Point
+
+    # Kurbo Library Source: https://github.com/linebender/kurbo/blob/884483b3de412c7c10e2fff4f43dbe96304c0dbd/src/line.rs#L44
+    def intersection(self, line: "Line", tolerance: float = 0.0):
+        lengthAB = sqrt(pow(self.p1.x - self.p0.x, 2) + pow(self.p1.y - self.p0.y, 2))
+        lengthCD = sqrt(pow(line.p1.x - line.p0.x, 2) + pow(line.p1.y - line.p0.y, 2))
+
+        normalized_ab = Point((self.p1.x - self.p0.x) / lengthAB, (self.p1.y - self.p0.y) / lengthAB)
+        normalized_cd = Point((line.p1.x - line.p0.x) / lengthCD, (line.p1.y - line.p0.y) / lengthCD)
+
+        a = Point(self.p1.x - normalized_ab.x * (lengthAB + tolerance), self.p1.y - normalized_ab.y * (lengthAB + tolerance))
+        b = Point(self.p0.x + normalized_ab.x * (lengthAB + tolerance), self.p0.y + normalized_ab.y * (lengthAB + tolerance))
+        c = Point(line.p1.x - normalized_cd.x * (lengthCD + tolerance), line.p1.y - normalized_cd.y * (lengthCD + tolerance))
+        d = Point(line.p0.x + normalized_cd.x * (lengthCD + tolerance), line.p0.y + normalized_cd.y * (lengthCD + tolerance))
+
+        ab = Point(b.x - a.x, b.y - a.y)
+        cd = Point(d.x - c.x, d.y - c.y)
+        ab_cross_cd = ab.x * cd.y - ab.y * cd.x
+
+        if ab_cross_cd == 0.0:
+            return None
+
+        ca = Point(a.x - c.x, a.y - c.y)
+        ab_cross_ca = ab.x * ca.y - ab.y * ca.x
+        cd_cross_ca = cd.x * ca.y - cd.y * ca.x
+        # h and g are the factors how much a point should be moved along the line.
+        g = cd_cross_ca / ab_cross_cd
+        h = ab_cross_ca / ab_cross_cd
+
+        # The values of g and h must be between 0 and 1, otherwise the point will
+        # be outside the lines.
+        if 0.0 <= g <= 1.0 and 0.0 <= h <= 1.0:
+            return Point(c.x + cd.x * h, c.y + cd.y * h)
+
+        return None
 
 
 @dataclass
