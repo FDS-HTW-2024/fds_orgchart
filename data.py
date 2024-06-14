bc13ea702c34b9a03df2a8c53497bf32c2577e15
hummingly <31522351+hummingly@users.noreply.github.com>
2024-06-14 12:06:34 +0200

    WIP

--- data.py
+++ data.py
@@ -1,50 +1,62 @@
 from dataclasses import dataclass
 from collections import namedtuple
 from math import sqrt
-import typing
+from typing import NamedTuple, Self
 
-Point = namedtuple("Point", ["x", "y"], defaults=[0.0, 0.0])
+class Point(NamedTuple):
+    x: float = 0.0
+    y: float = 0.0
 
+    def __add__(self, other: Self) -> Self:
+        return Point(self.x + other.x, self.y + other.y)
 
-class Rectangle(typing.NamedTuple):
+    def __sub__(self, other: Self) -> Self:
+        return Point(self.x - other.x, self.y - other.y)
+    
+    def distance(self) -> float:
+        return sqrt(self.x * self.x + self.y * self.y)
+
+class Rectangle(NamedTuple):
     """Represents a Rectangle in the orgchart"""
     top_left: Point
     bottom_right: Point
 
-    def inflate(self, value: float):
+    def inflate(self, value: float) -> Self:
         return Rectangle(
             top_left=Point(self.top_left.x - value, self.top_left.y - value),
             bottom_right=Point(self.bottom_right.x + value, self.bottom_right.y + value)
         )
 
 
-class Line(typing.NamedTuple):
+class Line(NamedTuple):
     p0: Point
     p1: Point
 
     # Kurbo Library Source: https://github.com/linebender/kurbo/blob/884483b3de412c7c10e2fff4f43dbe96304c0dbd/src/line.rs#L44
-    def intersection(self, line: "Line", tolerance: float = 0.0):
-        lengthAB = sqrt(pow(self.p1.x - self.p0.x, 2) + pow(self.p1.y - self.p0.y, 2))
-        lengthCD = sqrt(pow(line.p1.x - line.p0.x, 2) + pow(line.p1.y - line.p0.y, 2))
+    def intersection(self, line: Self, tolerance: float = 0.0) -> Self:
+        ab = self.p1 - self.p0
+        cd = line.p1 - line.p0
+        lengthAB = ab.distance()
+        lengthCD = cd.distance()
         newLengthAB = lengthAB + tolerance
         newLengthCD = lengthCD + tolerance
 
-        offset_ab = Point((self.p1.x - self.p0.x) / lengthAB * newLengthAB, (self.p1.y - self.p0.y) / lengthAB * newLengthAB)
-        offset_cd = Point((line.p1.x - line.p0.x) / lengthCD * newLengthCD, (line.p1.y - line.p0.y) / lengthCD * newLengthCD)
+        offset_ab = Point(ab.x / lengthAB * newLengthAB, ab.y / lengthAB * newLengthAB)
+        offset_cd = Point(cd.x / lengthCD * newLengthCD, cd.y / lengthCD * newLengthCD)
 
-        a = Point(self.p1.x - offset_ab.x, self.p1.y - offset_ab.y)
-        b = Point(self.p0.x + offset_ab.x, self.p0.y + offset_ab.y)
-        c = Point(line.p1.x - offset_cd.x, line.p1.y - offset_cd.y)
-        d = Point(line.p0.x + offset_cd.x, line.p0.y + offset_cd.y)
+        a = self.p1 - offset_ab
+        b = self.p0 + offset_ab
+        c = line.p1 - offset_cd
+        d = line.p0 + offset_cd
 
-        ab = Point(b.x - a.x, b.y - a.y)
-        cd = Point(d.x - c.x, d.y - c.y)
+        ab = b - a
+        cd = d - c
         ab_cross_cd = ab.x * cd.y - ab.y * cd.x
 
         if ab_cross_cd == 0.0:
             return None
 
-        ca = Point(a.x - c.x, a.y - c.y)
+        ca = a - c
         ab_cross_ca = ab.x * ca.y - ab.y * ca.x
         cd_cross_ca = cd.x * ca.y - cd.y * ca.x
         # h and g are the factors how much a point should be moved along the line.
