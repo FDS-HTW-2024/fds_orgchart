67efa3caed8132e5fbcb338ca10de57ed078666d
hummingly <31522351+hummingly@users.noreply.github.com>
2024-06-15 10:30:55 +0200

    Cleanup WIP Rectangle detection with sorted array functions

--- extract.py
+++ extract.py
@@ -48,114 +48,61 @@ def sorted_array_find_range[T](
 
     return array[start:end]
 
-# tolerance = 0.0
-# page = pymupdf.open("example_orgcharts/org_kultur.pdf")[0]
+tolerance = 0.0
+page = pymupdf.open("example_orgcharts/org_kultur.pdf")[0]
 
-# rectangles: list[Rectangle] = list()
-# lines: list[Line] = list()
+rectangles: list[Rectangle] = list()
+lines: list[Line] = list()
 
-# for drawing in page.get_drawings():
-#     for item in drawing["items"]:
-#         match item[0]:
-#             case "re":
-#                 rectangles.append(Rectangle(
-#                     top_left=Point._make(item[1].top_left),
-#                     bottom_right=Point._make(item[1].bottom_right)                 
-#                 ))
-#             case "l":
-#                 lines.append(Line(
-#                     p0=Point._make(item[1]),
-#                     p1=Point._make(item[2])                 
-#                 ))
-#             case _:
-#                 pass
+for drawing in page.get_drawings():
+    for item in drawing["items"]:
+        match item[0]:
+            case "re":
+                rectangles.append(Rectangle(
+                    top_left=Point._make(item[1].top_left),
+                    bottom_right=Point._make(item[1].bottom_right)                 
+                ))
+            case "l":
+                lines.append(Line(
+                    p0=Point._make(item[1]),
+                    p1=Point._make(item[2])                 
+                ))
+            case _:
+                pass
 
-# # Line intersecting with line
-# junctions = list()
-# # Line intersecting with rectangle
-# nodes = list()
+# Line intersecting with line
+junctions: list[tuple[int, int, Point]] = list()
+# Line intersecting with rectangle
+nodes = list()
 
-# lines.sort()
-# for l_i in range(0, len(lines)):
-#     line_i = lines[l_i]
+lines.sort()
+for l_i in range(0, len(lines)):
+    line_i = lines[l_i]
 
-#     for l_j in range(l_i + 1, len(lines)):
-#         line_j = lines[l_j]
+    for l_j in range(l_i + 1, len(lines)):
+        line_j = lines[l_j]
 
-#         intersection = line_i.intersection(line_j, tolerance)
+        intersection = line_i.intersection(line_j, tolerance)
 
-#         if intersection != None:
-#             junctions.append((l_i, l_j, intersection))
+        if intersection != None:
+            junctions.append((l_i, l_j, intersection))
 
-# # Search for rectangles made up of 4 lines
-# # TODO: range function to get only element with certain key
-# j_i = 0
-# for (l_i, intersections) in sorted_array_group_by(junctions, lambda j: j[0]):
-#     j_i += len(intersections)
+# Search for rectangles made up of 4 lines
+j_i = 0
+for (l_i, intersections) in sorted_array_group_by(junctions, lambda j: j[0]):
+    intersection_count = len(intersections)
+    j_i += intersection_count
 
-#     if len(intersections) >= 2:
-#         line_i = lines[l_i]
+    if intersection_count < 2:
+        continue
 
-#         for (_, l_j, intersection) in intersections:
-#             if (line_i.p0 - intersection).distance() <= tolerance:
-#                 iter = itertools.dropwhile(
-#                     lambda j: j[0] != l_j,
-#                     sorted_array_group_by(junctions[j_i:], lambda j: j[0])
-#                 )
-#                 for (k, g) in sorted_array_group_by(junctions[j_i:], lambda j: j[0]):
-#                     if k == l_j:
-#                         for (_, l_m, _) in g:
-                            
-#                         break
-#             if (line_i.p1 - intersection).distance() <= tolerance:
-#                 pass
+    line_i = lines[l_i]
 
-
-# # TODO: Write array group by generator
-# j_i = 1
-# while j_i < len(junctions):
-#     count = 1
-
-#     while junctions[j_i - 1][0] == junctions[j_i][0]:
-#         count += 1            
-#         j_i += 1
-
-#     if count >= 2:
-#         line_groups = dict()
-
-#         for (l_i, l_j, intersection) in junctions[j_i -count:j_i]:
-#             line_i = lines[l_i]
-
-#             if (line_i.p0 - intersection).distance() <= tolerance:
-#                 for (k, g) in itertools.groupby(junctions[j_i:], lambda j: j[0]):
-#                     if k == l_j:
-#                         for (_, l_m, _) in g:
-#                             value = line_groups.setdefault(l_m, (l_j, None))
-
-#                             if value[0] == None:
-#                                 line_groups[l_m] = (l_j, value[1])
-
-#                         break
-#             elif (line_i.p1 - intersection).distance() <= tolerance:
-#                 for (k, g) in itertools.groupby(junctions[j_i:], lambda j: j[0]):
-#                     if k == l_j:
-#                         for (_, l_m, _) in g:
-#                             value = line_groups.setdefault(l_m, (None, l_j))
-
-#                             if value[1] == None:
-#                                 line_groups[l_m] = (value[0], l_j)
-
-#                         break
-
-#         for (l_m, (l_j, l_k)) in line_groups.items():
-#             if l_j != None and l_k != None:
-                
-#                 # TODO: Continue
-        
-        
-#     j_i += 1
-        
-    
-# rectangles.sort()
-
+    for (_, l_j, intersection) in intersections:
+        if (line_i.p0 - intersection).distance() <= tolerance:
+            for (_, l_k0, _) in sorted_array_find_range(junctions[j_i:], lambda j: j[0] == l_j):
+                print(l_i, l_j, l_k0)
+        if (line_i.p1 - intersection).distance() <= tolerance:
+            for (_, l_k1, _) in sorted_array_find_range(junctions[j_i:], lambda j: j[0] == l_j):
+                print(l_i, l_j, l_k0)
 
