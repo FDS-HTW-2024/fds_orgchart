ad93237839dfd37c6f688e578301ab088db90b54
Niklas Lengert <s0563290@htw-berlin.de>
2024-07-03 20:51:20 +0200

    Squares and Lines done

--- src/organigram_extract/view/script.js
+++ src/organigram_extract/view/script.js
@@ -77,9 +77,9 @@ function createSVG(data, identifier) {
 
     // Create an SVG element
     const svg = document.createElementNS(svgNamespace, 'svg');
-    svg.setAttribute('width', '400');
-    svg.setAttribute('height', '400');
-    svg.setAttribute('viewBox', '0 0 400 400');
+    svg.setAttribute('width', '1200');
+    svg.setAttribute('height', '600');
+    svg.setAttribute('viewBox', '0 0 600 1200');
 
     // Example: Create circles from JSON data
     if(identifier == 1){
@@ -112,21 +112,34 @@ function createSVG(data, identifier) {
     } else if(identifier == 4){
         data.rectangles1.forEach(rect => {
             const rectElement = document.createElementNS(svgNamespace, 'rect');
-            const text = document.createElementNS(svgNamespace, 'text')
+            const text = document.createElementNS(svgNamespace, 'text') 
             const textX = rect.x + Math.abs(rect.x - rect.x2)/2;
             const textY = rect.y + Math.abs(rect.y - rect.y2)/2;
             rectElement.setAttribute('x', rect.x);
             rectElement.setAttribute('y', rect.y);
             rectElement.setAttribute('width', Math.abs(rect.x - rect.x2));
             rectElement.setAttribute('height', Math.abs(rect.y - rect.y2));
-            rectElement.setAttribute('fill', rect.fill);
+            rectElement.setAttribute('fill', rect.fill)
+            rectElement.setAttribute('stroke', "black")
+            rectElement.setAttribute('stroke-width', 3)
             text.setAttribute('x', textX)
             text.setAttribute('y', textY)
             text.setAttribute('text-anchor', 'middle');
             text.setAttribute('dominant-baseline', 'middle');
-            text.textContent = rect.text;
+            text.textContent = rect.text; 
             svg.appendChild(rectElement)
-            svg.appendChild(text)
+            svg.appendChild(text) 
+        })
+
+        data.lines1.forEach(line => {
+            const lineElement = document.createElementNS(svgNamespace, 'line')
+            lineElement.setAttribute('x1', line.x)
+            lineElement.setAttribute('y1', line.y)
+            lineElement.setAttribute('x2', line.x2)
+            lineElement.setAttribute('y2', line.y2)
+            lineElement.setAttribute('stroke', 'black')
+            lineElement.setAttribute('stroke-width', 2)
+            svg.appendChild(lineElement)
         })
 
     }
