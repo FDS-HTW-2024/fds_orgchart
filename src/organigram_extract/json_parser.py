6612e49912d7c02b9b3634faa32c311efc3d4f6d
Niklas Lengert <s0563290@htw-berlin.de>
2024-07-11 23:33:23 +0200

    content nodes parsing to json

--- src/organigram_extract/json_parser.py
+++ src/organigram_extract/json_parser.py
@@ -1,6 +1,20 @@
 import json
+from dataclasses import asdict, is_dataclass
+from organigram_extract.data import Rect, TextLine, ContentNode
 from organigram_extract.extract import extract
 import pymupdf
+
+def dataclass_to_dict(obj):
+    if is_dataclass(obj):
+        result = asdict(obj)
+        for key, value in result.items():
+            if isinstance(value, list):
+                result[key] = [dataclass_to_dict(item) for item in value]
+            elif is_dataclass(value):
+                result[key] = dataclass_to_dict(value)
+        return result
+    else:
+        return obj
 
 def json_parser(filename: str, tolerance: float = 1.0):
 
@@ -11,16 +25,24 @@ def json_parser(filename: str, tolerance: float = 1.0):
 
      # Extract data from the PDF using the extract function
     rects, lines, junction_by_line, text, content_nodes = extract(page, tolerance)
+    
+    content_nodes_blocks = []
+    for node in content_nodes:
+        for block_index, data_block in enumerate(node.block):
+            data_block = node.block[block_index]
+            content_nodes_blocks.append(data_block)
+
+    content_nodes_blocks_dicts = [dataclass_to_dict(block) for block in content_nodes_blocks]
 
     # Organize the extracted data into dictionaries
-    #content_nodes_data = {"contentNodes": content_nodes}
+    content_nodes_data = {"content_nodes_data": content_nodes_blocks_dicts}
     rects_data = {"rectangles": rects}
     lines_data = {"lines": lines}
     
     # Convert the dictionaries to JSON strings with indentation for readability
     rects_json = json.dumps(rects_data, indent=4)
     lines_json = json.dumps(lines_data, indent=4)
-    #content_nodes_json = json.dumps(content_nodes_data, indent=4)
+    content_nodes_json = json.dumps(content_nodes_data, ensure_ascii=False, indent=4)
     
     # Write the JSON strings to files
     with open(f'{output_path}/rects.json', 'w') as rects_file:
@@ -29,8 +51,8 @@ def json_parser(filename: str, tolerance: float = 1.0):
     with open(f'{output_path}/lines.json', 'w') as lines_file:
         lines_file.write(lines_json)
 
-    # with open(f'{output_path}/content_nodes.json', 'w') as content_nodes_file:
-    #     content_nodes_file.write(content_nodes_json)
+    with open(f'{output_path}/content_nodes.json', 'w', encoding='utf-8') as content_nodes_file:
+        content_nodes_file.write(content_nodes_json)
 
 # Example usage
 json_parser("../../example_orgcharts/org_inneres.pdf")
