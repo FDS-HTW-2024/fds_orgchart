ca03f8d64908d09788ccfc23ea8c41cff75e9662
Niklas Lengert <s0563290@htw-berlin.de>
2024-07-15 22:01:05 +0200

    js changes

--- src/organigram_extract/view/script.js
+++ src/organigram_extract/view/script.js
@@ -1,120 +1,68 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const tabsContainer = document.getElementById("tabsContainer");
-
-    let count = 10
-
-
-    for(let i = 0; i <= count; i++){
-        const newTab = document.createElement("button");
-        newTab.classList.add("tab");
-        newTab.setAttribute('id',i);
-        newTab.innerHTML = "test";
-        newTab.id
-        if(Number(newTab.id)=== 0){
-            newTab.classList.add("tab_active");
-        }
-
-        newTab.addEventListener("click", function(){
-            handleTab(i)
-            document.querySelectorAll('.tab').forEach(tab => tab.classList.remove("tab_active"));
-            newTab.classList.add("tab_active");
-        });
-
-        tabsContainer.appendChild(newTab);
-    }
-
-    handleTab(0);
-
-})
-function handleTab(tabIndex){
-    document.getElementById('svgContainer').innerHTML = '';
-
-    fetch('data.json')
+function createTabs(){
+    document.addEventListener("DOMContentLoaded", function(){
+        fetch('data.json')
         .then(response => response.json())
         .then(data => {
             createSVG(data, tabIndex);
         })
         .catch(error => console.error('Error fetching JSON:', error));
+        const tabsContainer = document.getElementById("tabsContainer");
+        const result = countFileNameOccurrences(data);
 
-        fetch('data.json')
-        .then(response => response.json())
-        .then(data2 => {
-            createSVG(data2, tabIndex);
-        })
-        .catch(error => console.error('Error fetching data.json:', error));
+        for(let i = 0; i <= result.count; i++){
+            const newTab = document.createElement("button");
+            newTab.classList.add("tab");
+            newTab.setAttribute('id',i);
+            setTabName(i, result.values[i])
+            newTab.id
+            if(Number(newTab.id)=== 0){
+                newTab.classList.add("tab_active");
+            }
 
-/*document.addEventListener('DOMContentLoaded', function () {
-    fetch('../../../json_holder/rects.json')
-        .then(response => response.json())
-        .then(data => {
-            createSVG(data);
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
+            newTab.addEventListener("click", function(){
+                handleTab(i)
+                document.querySelectorAll('.tab').forEach(tab => tab.classList.remove("tab_active"));
+                newTab.classList.add("tab_active");
+            });
 
-    const firstBtn = document.getElementById('first');
-    const secondBtn = document.getElementById('second');
-    const thirdBtn = document.getElementById('third');
+            tabsContainer.appendChild(newTab);
+        }
 
-    firstBtn.addEventListener('click', function() {
+        handleTab(0);
 
-        firstBtn.classList.add("tab_active")
-        firstBtn.classList.remove("tab")
+    })
+}
 
-        secondBtn.classList.remove("tab_active")
-        secondBtn.classList.add("tab")
-
-        thirdBtn.classList.remove("tab_active")
-        thirdBtn.classList.add("tab")
-
-        document.getElementById('svgContainer').innerHTML = '';
-        fetch('../../../json_holder/rects.json')
-        .then(response => response.json())
-        .then(data => {
-            createSVG(data, 1);
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
-    });
-
-    secondBtn.addEventListener('click', function() {
-
-        firstBtn.classList.remove("tab_active")
-        firstBtn.classList.add("tab")
-
-        thirdBtn.classList.remove("tab_active")
-        thirdBtn.classList.add("tab")
-
-        secondBtn.classList.remove("tab")
-        secondBtn.classList.add("tab_active")
-
-        document.getElementById('svgContainer').innerHTML = '';
-        fetch('../../../json_holder/rects.json')
-        .then(response => response.json())
-        .then(data => {
-            createSVG(data, 2);
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
-    });
-    thirdBtn.addEventListener('click', function() {
-
-        firstBtn.classList.remove("tab_active")
-        firstBtn.classList.add("tab")
-
-        secondBtn.classList.remove("tab_active")
-        secondBtn.classList.add("tab")
-
-        thirdBtn.classList.remove("tab")
-        thirdBtn.classList.add("tab_active")
-
-        document.getElementById('svgContainer').innerHTML = '';
-        fetch('../../../json_holder/rects.json')
-        .then(response => response.json())
-        .then(data => {
-            createSVG(data, 4);
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
-    });
-});*/
+function setTabName(tabIndex, nameParameter){
+    document.getElementById('svgContainer').innerHTML = '';
+    tabName = nameParameter.values[tabIndex];
+    newTab.innerHTML = tabName;
 };
+
+function countFileNameOccurrences(obj) {
+    let count = 0;
+    let values = [];
+
+    function recursiveSearch(currentObj) {
+        for (const key in currentObj) {
+            if (currentObj.hasOwnProperty(key)) {
+                const value = currentObj[key];
+                if (key === "fileName") {
+                    count++;
+                    values.push(value);
+                }
+                if (typeof value === 'object' && value !== null) {
+                    recursiveSearch(value);
+                }
+            }
+        }
+    }
+
+    recursiveSearch(obj);
+
+    return { count, values };
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const tabs = document.querySelectorAll('.tab');
     const root = document.documentElement;
@@ -127,49 +75,37 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
 function createSVG(data, identifier) {
     const svgNamespace = "http://www.w3.org/2000/svg";
     const svgContainer = document.getElementById('svgContainer');
-
 
     // Create an SVG element
     const svg = document.createElementNS(svgNamespace, 'svg');
     svg.setAttribute('width', '1200');
     svg.setAttribute('height', '600');
-    svg.setAttribute('viewBox', '0 0 600 1200');
+    svg.setAttribute('viewBox', '0 0 1200 600');
 
     // Example: Create circles from JSON data
-    if(identifier == 1){
-        data.rectangles.forEach(rect => {
-            const rectElement = document.createElementNS(svgNamespace, 'rect');
-            const [x, y, x2, y2] = rect;
-            console.log("X2: " + x2 + ", Y2: " + y2)
-            const width = Math.round(Math.abs(x - x2), 2)
-            const height = Math.round(Math.abs(y - y2), 2)
-            console.log("width: " + width + ", heigth: " + height)
-            rectElement.setAttribute('x', x);
-            rectElement.setAttribute('y', y);
-            rectElement.setAttribute('width', width);
-            rectElement.setAttribute('height', height);
-            rectElement.setAttribute('fill', "white")
-            rectElement.setAttribute('stroke', "black")
-            rectElement.setAttribute('stroke-width', 3)
-            svg.appendChild(rectElement);
+    if(identifier == 0){
+        data.content.forEach(item => {
+            const bbox = item.bbox;
+            const rect = document.createElementNS(svgNamespace, 'rect');
+            rect.setAttribute('x', bbox[0]);
+            rect.setAttribute('y', bbox[1]);
+            rect.setAttribute('width', bbox[2] - bbox[0]);
+            rect.setAttribute('height', bbox[3] - bbox[1]);
+            rect.setAttribute('stroke', "black")
+            rect.setAttribute('fill', "white")
+            rect.setAttribute('stroke-width', 1)
+            svg.appendChild(rect);
         })
-        /*data.lines1.forEach(line => {
-            const lineElement = document.createElementNS(svgNamespace, 'line')
-            lineElement.setAttribute('x1', line.x)
-            lineElement.setAttribute('y1', line.y)
-            lineElement.setAttribute('x2', line.x2)
-            lineElement.setAttribute('y2', line.y2)
-            lineElement.setAttribute('stroke', 'black')
-            lineElement.setAttribute('stroke-width', 2)
-            svg.appendChild(lineElement)
-        })*/
-
     }
 
     // Append the SVG to the container
     svgContainer.appendChild(svg);
 }
+
+createTabs();
+
 
