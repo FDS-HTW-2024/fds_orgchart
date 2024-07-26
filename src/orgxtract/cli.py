51a44135300d22101e1b7d4afdfb335eb1c41da6
hummingly <31522351+hummingly@users.noreply.github.com>
2024-07-27 00:15:06 +0200

    Nicer cli args and make log level configurable

--- src/orgxtract/cli.py
+++ src/orgxtract/cli.py
@@ -12,22 +12,26 @@ from orgxtract import Document, Drawing, TextPipeline
 import orgxtract.pdf as pdf
 
 def run():
-    parser = argparse.ArgumentParser(prog="Organigramm Extract")
+    parser = argparse.ArgumentParser(prog=__package__)
 
-    parser.add_argument("input_path",
+    parser.add_argument("input",
                         help="source file or directory containing source files for extraction")
-    parser.add_argument("-o", "--output_path",
-                        help="output file or directory to write the extracted content to")
+    parser.add_argument("-o", "--output",
+                        help="target file or directory to write the extracted content to")
     parser.add_argument("-m", "--model",
-                        help="LLM to use for content extraction")
+                        help="name of LLM to use for content extraction")
     parser.add_argument("-k", "--key",
-                        help="specify API key for LLM")
-    parser.add_argument("-d", "--data_path",
+                        help="API key of LLM")
+    parser.add_argument("-d", "--data-path",
                         help="path containing files to override data files (e.g. schema.json)")
-    parser.add_argument("-w", "--worker_threads",
+    parser.add_argument("-w", "--worker-threads",
                         help="max amount of spawned threads for page extraction and LLM tasks",
                         type=int,
                         default=4)
+    parser.add_argument("--log-level",
+                        help="logging level",
+                        choices=["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+                        default="INFO")
 
     args = parser.parse_args()
     config = {key:value for (key, value) in vars(args).items() if value != None}
@@ -35,7 +39,7 @@ def run():
     if args.key == None and "API_KEY" in os.environ:
         config["key"] = os.environ["API_KEY"]
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=args.log_level)
 
     executor = ThreadPoolExecutor(max_workers=args.worker_threads)
     task_queue = Queue(1)
@@ -47,25 +51,25 @@ def run():
         # much memory accidentally.
         text_processing = executor.submit(process_text, task_queue, config)
 
-        input_path = args.input_path
-        output_path = args.output_path
+        input = args.input
+        output = args.output
 
-        if os.path.isfile(input_path):
-            process_file(executor, task_queue, input_path, output_path)
-        elif os.path.isdir(args.input_path):
-            for filename in os.listdir(args.input_path):
-                input_file = os.path.join(input_path, filename)
-                output_file = output_path
+        if os.path.isfile(input):
+            process_file(executor, task_queue, input, output)
+        elif os.path.isdir(input):
+            for filename in os.listdir(input):
+                input_file = os.path.join(input, filename)
+                output_file = output
 
-                if output_path != None:
+                if output != None:
                     (name, _) = os.path.splitext(filename)
-                    output_file = os.path.join(output_path, name + ".json")
+                    output_file = os.path.join(output, name + ".json")
                    
                 process_file(executor, task_queue, input_file, output_file)
         else:
             raise FileNotFoundError(errno.ENOENT,
                                     os.strerror(errno.ENOENT),
-                                    input_path)
+                                    input)
     finally:
         executor.shutdown(wait=False, cancel_futures=True)
         # Signals text processing thread to shutdown
