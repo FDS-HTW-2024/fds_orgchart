dc90d4cf24df643b21fb296991fefc9c55dc40c0
hummingly <31522351+hummingly@users.noreply.github.com>
2024-08-02 01:05:15 +0200

    Adding license note to method to be on the safe side

--- src/orgxtract/drawing.py
+++ src/orgxtract/drawing.py
@@ -49,8 +49,28 @@ class Line(NamedTuple):
     p0: Point
     p1: Point
 
-    # Kurbo Library Source: https://github.com/linebender/kurbo/blob/884483b3de412c7c10e2fff4f43dbe96304c0dbd/src/line.rs#L44:c
     def intersection(self, line: Self, tolerance: float) -> Optional[Self]:
+        # Kurbo Library Source: https://github.com/linebender/kurbo/blob/884483b3de412c7c10e2fff4f43dbe96304c0dbd/src/line.rs#L44:c
+        #
+        # Copyright (c) 2018 Raph Levien
+
+        # Permission is hereby granted, free of charge, to any person obtaining a copy
+        # of this software and associated documentation files (the "Software"), to deal
+        # in the Software without restriction, including without limitation the rights
+        # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        # copies of the Software, and to permit persons to whom the Software is
+        # furnished to do so, subject to the following conditions:
+
+        # The above copyright notice and this permission notice shall be included in all
+        # copies or substantial portions of the Software.
+
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
         a = self.p1
         b = self.p0
         c = line.p1
